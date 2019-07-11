a26d1f7178fb01e338ecc39c88144c755e4331f2
greg higgins <greg.higgins@v12technology.com>
2019-07-11 10:52:55 +0100

    code for serialised lambda article

--- 2019/july/lambdaserialiser/src/main/java/com/fluxtion/articles/lambdaserialiser/Main.java
+++ 2019/july/lambdaserialiser/src/main/java/com/fluxtion/articles/lambdaserialiser/Main.java
@@ -16,23 +16,75 @@
  */
 package com.fluxtion.articles.lambdaserialiser;
 
+import com.fluxtion.api.partition.LambdaReflection.SerializableFunction;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.util.function.Function;
 
 /**
+ * Example demonstrating serialising a lambda function to a file for a later
+ * invocation. Running the application will :
  *
- * @author V12 Technology Ltd.
+ *
+ * <ul>
+ * <li>serialise the lambda to a file
+ * <li>deserialise the lambda and execute it
+ * <li>delete the file
+ * <li>fail to deserialise lambda and replace with a new version
+ * <li>deserialise the new lambda and execute it with a different output
+ * </ul>
+ *
+ * Set the {@code
+ * serialiseOriginal = false
+ * }
+ * to demonstrate the application running with whatever the last saved state of
+ * the lambda was.
+ *
+ * @author greg.higgins@v12technology.com
  */
 public class Main {
-    
-    
-    public static void main(String[] args) {
-        serialise(s -> 1, "func1");
-        serialise(s -> (int)System.currentTimeMillis(), "funcRandom");
+
+    private static final String ROOT_DIR = "src/main/resources/lambda.ser";
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        boolean serialiseOriginal = true;
+        if (serialiseOriginal) {
+            serialise((String s) -> "hello " + s, "funcTest-1");
+        }
+        Function funcDesr = deserialise("funcTest-1");
+        System.out.println("funcTest-1: " + funcDesr.apply("serialised lambda"));
+
+        //now fail by deleting file, catch and replace with new lambda 
+        Files.delete(new File(ROOT_DIR, "funcTest-1").toPath());
+        try {
+            funcDesr = deserialise("funcTest-1");
+        } catch (IOException | ClassNotFoundException iOException) {
+            serialise((String s) -> "new version of hello " + s, "funcTest-1");
+        }
+        //successfully execute
+        funcDesr = deserialise("funcTest-1");
+        System.out.println("funcTest-1: " + funcDesr.apply("serialised lambda"));
     }
-    
-    
-    public static String serialise(Function<String, Integer> f, String name){
-        return "";
+
+    public static <T, R> void serialise(SerializableFunction<T, R> f, String name) throws IOException {
+        final File outDir = new File(ROOT_DIR);
+        Files.createDirectories(outDir.toPath());
+        OutputStream os = new FileOutputStream(new File(outDir, name));
+        try (ObjectOutputStream ois = new ObjectOutputStream(os)) {
+            ois.writeObject(f);
+        }
     }
-    
+
+    public static <T, R> Function<T, R> deserialise(String lambdaName) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File(ROOT_DIR, lambdaName)))) {
+            return (Function<T, R>) ois.readObject();
+        }
+    }
+
 }
